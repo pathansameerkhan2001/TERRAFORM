a1d8918da0d4d8429d1504e190d4370050b37c18
Sameer <sameerk0601@gmail.com>
2023-02-17 14:12:32 +0530

    Update demo.tf
    new

--- demo.tf
+++ demo.tf
@@ -9,8 +9,8 @@ terraform {
 
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIAXA6BXUMT7KCRUW7Z"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
 
 #creating vpc 
@@ -26,7 +26,7 @@ resource "aws_vpc" "my-vpc" {
 #creating internet gate way
 
 resource "aws_internet_gateway" "demo-gateway" {
-  vpc_id = "vpc-06a02ecff9b4b756a"
+  vpc_id = ""
 
   tags = {
     Name = "demo-gateway"
@@ -36,11 +36,11 @@ resource "aws_internet_gateway" "demo-gateway" {
 #custom route table 
 
 resource "aws_route_table" "sample-route-table" {
-  vpc_id = "vpc-06a02ecff9b4b756a"
+  vpc_id = ""
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = "igw-03617183a0138ee06"
+    gateway_id = ""
   }
 
   tags = {
@@ -53,7 +53,7 @@ resource "aws_route_table" "sample-route-table" {
 #creating the Subnet 
 
 resource "aws_subnet" "new-subnet" {
-  vpc_id     = "vpc-06a02ecff9b4b756a"
+  vpc_id     = ""
   cidr_block = "10.0.1.0/24"     
 
   tags = {
@@ -64,15 +64,15 @@ resource "aws_subnet" "new-subnet" {
 #Associting the Subnet with the Route Table
 
 resource "aws_route_table_association" "new-srt" {
-  subnet_id      = "subnet-09def1f32d6d5c124"
-  route_table_id = "rtb-01970ef70dcf9d752"
+  subnet_id      = ""
+  route_table_id = ""
 }
 
 #creating new security group
 
 resource "aws_security_group" "demo_sg" {
   name_prefix = "demo_sg"
-  vpc_id      = "vpc-06a02ecff9b4b756a"
+  vpc_id      = ""
 
   ingress {
     from_port   = 22
@@ -110,9 +110,9 @@ resource "aws_security_group" "demo_sg" {
 #creating network_interface  
 
 resource "aws_network_interface" "demo_network" {
-  subnet_id       = "subnet-09def1f32d6d5c124"
+  subnet_id       = ""
   private_ips     = ["10.0.1.10"]
-  security_groups = ["sg-0c32db1d22a95ecf0"]
+  security_groups = [""]
 
   tags = {
     Name = "demo_network"
@@ -126,18 +126,18 @@ resource "aws_eip" "new_eip" {
 }
 
 resource "aws_eip_association" "new_eip" {
-  network_interface_id = "eni-00cbfd25dd91e30b3"
-  allocation_id        = "eipalloc-08c83d1bba95d4a17"
+  network_interface_id = ""
+  allocation_id        = ""
 }
 
 #creating aws instance
 
 resource "aws_instance" "demo" {
-  ami           = "ami-0caf778a172362f1c"
+  ami           = ""
   instance_type = "t2.micro"
-  key_name = "sameer123"
-  vpc_security_group_ids = ["sg-0c32db1d22a95ecf0"]
-  subnet_id = "subnet-09def1f32d6d5c124"
+  key_name = ""
+  vpc_security_group_ids = [""]
+  subnet_id = ""
   associate_public_ip_address = true
 
   tags = {
